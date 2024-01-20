de6e2ba25abe33e47461fb22fbc3f3a0d0db1a36
Rahul Tekale <141483386+rahultekale@users.noreply.github.com>
2024-01-21 00:00:27 +0530

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function page9 (){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page9.html","_self")
+}
+function page8(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page8.html","_self")
+}
+
+function page7(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page7.html","_self")
+}
+
+function page6(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page6.html","_self")
+}
+
+function page5(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page5.html","_self")
+}
+
+function page4(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page4.html","_self")
+}
+
+function page3(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page3.html","_self")
+}
+
+function page2(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/page2.html","_self")
+}
+
+function page1(){
+    window.open("file:///C:/Users/Rahul%20Tekale/Desktop/WEB%20DEVLOPMENT/PROJECTS/intershala%20fly%20web%20dev/index.html","_self")
+}
+
+
+
